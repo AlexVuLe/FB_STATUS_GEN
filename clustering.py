7df58c692f33e5c6ebd9cb69b2e0606524b65bc0
Heidi Chen <s.heidi.chen@gmail.com>
2013-12-03 16:31:16 -0500

    find nodes and edgeweights for processed status

---
+++ clustering.py
@@ -0,0 +1,50 @@
+import pickle
+import pandas as pd
+import itertools
+
+input_file = 'Data/heidi_processed_status.p'
+
+data = pickle.load(open(input_file, 'rb'))
+
+'''
+class pair:
+    def __init__(self, word1, word2):
+        self.word1 = word1
+        self.word2 = word2
+    
+    def to_sorted_list(self):
+        return [self.word1, self.word2].sort()
+
+    def __eq__(self, other):
+        return (self.word1 == other.word1 and self.word2 == other.word2) or (self.word1 == other.word2 and self.word2 == other.word1)
+
+    def __str__(self):
+        return str((self.word1, self.word2))
+
+    def __repr__(self):
+        return str((self.word1, self.word2))
+'''
+
+def calculate_edgeweight(data):
+    # generate a dataframe of unique word-pairs, the cumulative number of likes, and the number of statuses that contain each word-pair
+    colnames = ['pair', 'word1', 'word2', 'cum_likes', 'count']
+    df = pd.DataFrame(columns = colnames)
+
+    for status in data:
+        num_likes  = status[0]
+        # all combinations of word-pairs in a given status
+        word_pairs = itertools.combinations(status[1], 2)
+        for word_pair in word_pairs:
+            current_pair = [word_pair[0], word_pair[1]]
+            current_pair.sort()
+            if word_pair in df['pair']:
+                df['cum_likes'][df['pair'] == word_pair] += num_likes
+                df['count'][df['pair'] == word_pair] += 1
+
+            else:
+                df = df.append(pd.Series([current_pair, current_pair[0], current_pair[1], num_likes, 1], index = colnames), ignore_index = True)
+                
+    return df
+
+df = calculate_edgeweight(data)
+df['avg_likes'] = df['cum_likes']/df['count']
