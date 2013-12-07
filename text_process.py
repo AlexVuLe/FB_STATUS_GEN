cf64c89a21a2faadf86b25c62b877136b59bb435
Alex Vu Le <vu.le.williams@gmail.com>
2013-12-07 17:22:49 -0500

    add comment

--- text_process.py
+++ text_process.py
@@ -2,6 +2,8 @@
 # Go to python, and input nltk.download()
 # Download 'stopwords', 'chat_words', and 'words' under corpus
 # Also need to download enchant by running 'sudo pip install pyenchant'
+# If you are using MacOS, unzip the pyenchant egg file in the required_pkgs
+# and copy the folder enchant into your Python site-packages folder
 
 import difflib
 import enchant
@@ -15,10 +17,10 @@ from nltk.stem.lancaster import LancasterStemmer
 from nltk.stem.porter import PorterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 
-# First removes punctuations and numbers. Then removes the list of stop words 
-# given from nltk. Then uses the lemmatizer then the Lancaster stemmer in 
+# First removes punctuations and numbers. Then removes the list of stop words
+# given from nltk. Then uses the lemmatizer then the Lancaster stemmer in
 # order to get the root of each words. Lemmatizer forms actual words
-# from the word list dictionary so the words it forms are actual words, 
+# from the word list dictionary so the words it forms are actual words,
 # but it doesn't get rid of every stems. However, the Lancaster removes
 # more stems, but it doesn't check with a dictionary and many of the words
 # changed are not actual words.
@@ -29,7 +31,7 @@ def remove_stems(file):
     stemmer = LancasterStemmer()
 
     for raw_post in file:
-        post = raw_post[1]        
+        post = raw_post[1]
         token = nltk.word_tokenize(post)
         new_token = []
         for word in token:
@@ -39,12 +41,12 @@ def remove_stems(file):
             # Stems each word to their roots, but using lemmatizer then Lancaster
             word = lemmatizer.lemmatize(original_word)
             if original_word == word:
-                word = stemmer.stem(word) 
-            
+                word = stemmer.stem(word)
+
             # Removes stopwords that are defined in the nltk library
             if word not in nltk.corpus.stopwords.words('english') and word != '':
                 new_token.append(word)
-        
+
         new_file.append((raw_post[0], new_token))
     return new_file
 
@@ -64,15 +66,15 @@ def find_stop_words(file, lower_bound, upper_bound):
 
     stop_words += frequency.keys()[-int(lower_bound):]
     stop_words += frequency.keys()[:int(upper_bound)]
-    
+
     return stop_words
 
-# Uses Enchant to correct misspellings. Use the enchant english dictionary 
-# and the chat words dictionary as reference. 
+# Uses Enchant to correct misspellings. Use the enchant english dictionary
+# and the chat words dictionary as reference.
 def correct_misspelling(word):
     pwl = enchant.request_pwl_dict(nltk.corpus.nps_chat.words())
     dict = enchant.DictWithPWL("en_US", 'nltk.corpus.nps_chat.words()')
-    
+
     # Ignores words with symbols/numbers since they are probably not regular words
     punctuation = re.compile(r'[-~ /#<>|0-9]')
     if not re.search(punctuation, word) == None: return word
@@ -81,7 +83,7 @@ def correct_misspelling(word):
     if not dict.check(word):
         suggestions = dict.suggest(word)
         if suggestions == []: return word
-        
+
         # Returns the closest match to the suggestion list using difflib
         suggested_word = difflib.get_close_matches(word, suggestions, 1)
 
@@ -91,10 +93,10 @@ def correct_misspelling(word):
 
 # The main method that runs text processing
 def process_file(file_name):
-    file = pickle.load(open(file_name, "rb")) 
+    file = pickle.load(open(file_name, "rb"))
     file = remove_stems(file)
     #stop_words = find_stop_words(file,.03,.97)
-    file_minus_stop_words = []    
+    file_minus_stop_words = []
 
     # Removes the words from stop_words and fixes misspellings
     for post in file:
