e3f7ff7332201e9ff51df96a9203b3075bd1450f
Alex Vu Le <vu.le.williams@gmail.com>
2013-12-08 22:47:23 -0500

    commetns

--- facebook_status.py
+++ facebook_status.py
@@ -6,8 +6,10 @@ Created on Nov 24, 2013
 import yaml
 import facebook
 import pickle
+import sys
 
 def get_user_token(file = 'user_access_token.yaml'):
+    '''Read authorized user token'''
     with open(file, 'r') as file:
         doc = yaml.load(file)
     user_token = doc['token']
@@ -15,11 +17,12 @@ def get_user_token(file = 'user_access_token.yaml'):
     return user_token
 
 def get_user_data():
+    '''Pull Facebook statuses and associated number of likes from Facebook GraphAPI'''
     user_token = get_user_token()
     graph = facebook.GraphAPI(user_token)
     data = []
     
-    offset = 0
+    offset = 0 #Facebook only displays 100 statuses per page so need to loop through
     statuses = graph.get_object('me/statuses', limit=100, offset=offset)['data']
     
     while len(statuses) > 0:
@@ -34,11 +37,11 @@ def get_user_data():
         statuses = graph.get_object('me/statuses', limit=100, offset=offset)['data']
     return data
 
-def main():
-    f = open('Data/heidi_status.p', 'wb')
+def main(filename):
+    f = open('Data/' + filename, 'wb')
     print get_user_data()
     pickle.dump(get_user_data(), f)
     
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
         
