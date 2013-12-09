b05cd1f69a611a55391af93744e318ac94255482
Heidi Chen <s.heidi.chen@gmail.com>
2013-12-08 19:29:13 -0500

    more clustering and plots

--- clustering.py
+++ clustering.py
@@ -5,8 +5,9 @@ import numpy as np
 import scipy.linalg as spl
 from scipy.cluster.vq import kmeans, whiten, vq
 from pylab import plot,show
+import networkx as nx
 
-input_file = 'Data/vu_processed_status.p'
+input_file = 'Data/vu_processed_status_tags.p'
 
 data = pickle.load(open(input_file, 'rb'))
 
@@ -37,7 +38,7 @@ def calculate_edgeweight(data):
         # all combinations of word-pairs in a given status
         word_pairs = itertools.combinations(status[1], 2)
         for word_pair in word_pairs:
-            current_pair = [word_pair[0], word_pair[1]]
+            current_pair = [word_pair[0][0], word_pair[1][0]]
             current_pair.sort()
             current_pair = tuple(current_pair)
             if current_pair in df['pair'].tolist():
@@ -55,7 +56,7 @@ def calculate_edgeweight(data):
 def build_graph(df):
     # construct the graph with a dataframe of unique word-pairs and edge-weights
     words = list(set(df['word1']) | set(df['word2']))
-    graph_df = pd.DataFrame(columns = words, index = words).fillna(0)
+    graph_df = pd.DataFrame(columns = words, index = words)
     for row, edge in df.T.iteritems():
         graph_df[edge['word1']][edge['word2']] = edge['avg_likes']
         graph_df[edge['word2']][edge['word1']] = edge['avg_likes']
@@ -85,13 +86,13 @@ def clustering(graph_df):
 df = calculate_edgeweight(data)
 df['avg_likes'] = df['cum_likes']/df['count']
 graph_df = build_graph(df)
-clusters = clustering(graph_df)
+#clusters = clustering(graph_df)
 
 
 
 # density based on top 10 weights
 def clustering_max_w(graph_df):
-    num_words = 15
+    num_words = 16
     max_pair_dict = {}
     max_likes = pd.Series(index = graph_df.index)
     for word in graph_df.index:
@@ -111,34 +112,47 @@ df = calculate_edgeweight(data)
 df['avg_likes'] = df['cum_likes']/df['count']
 graph_df = build_graph(df)
 #clusters = clustering(graph_df)
-clusters = clustering_max_w(graph_df)
-
+best_words = clustering_max_w(graph_df)
 
 
 # DBSCAN
 def dbscan_clustering(graph_df):
-    graph_df2 = 22 - graph_df.ix.as_matrix()
-    db = sklearn.cluster.DBSCAN(eps = 20, min_samples = 5).fit(graph_df2)
+    graph_df2 = graph_df.replace(0, np.nan)
+    db = sklearn.cluster.DBSCAN(eps = 10, min_samples = 5).fit(20/graph_df2)
     unique_labels = set(labels)
     colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
-    for k, col in zip(unique_labels, colors):
+    for k, col in zip(unique_labels, colors):%pas
         if k == -1:
             # Black used for noise.
             col = 'k'
-        markersize = 4
-    class_members = [index[0] for index in np.argwhere(labels == k)]
-    cluster_core_samples = [index for index in core_samples
-                            if labels[index] == k]
-    for index in class_members:
-        x = graph_df2[index]
-        if index in core_samples and k != -1:
-            markersize = 10
-        else:
             markersize = 4
-        plt.plot(x[0], x[1], 'o', markerfacecolor=col,
-                 markeredgecolor='k', markersize=markersize)
+        class_members = [index[0] for index in np.argwhere(labels == k)]
+        cluster_core_samples = [index for index in core_samples
+                                if labels[index] == k]
+        for index in class_members:
+            x = graph_df2[index]
+            if index in core_samples and k != -1:
+                markersize = 10
+            else:
+                markersize = 4
+            plt.plot(x[0], x[1], 'o', markerfacecolor=col,
+                     markeredgecolor='k', markersize=markersize)
 #        plt.xlim(0, 22)
 #       plt.ylim(0, 22)
-
+            
     plt.title('Estimated number of clusters: %d' % n_clusters_)
     plt.show()
+
+
+G = nx.Graph()
+for node in best_words:
+    G.add_node(node)
+smaller_graph = graph_df.loc[best_words][list(best_words)]
+for row, edge in smaller_graph.T.iteritems():
+    G.add_weighted_edges_from([(edge.name, column, edge[column]) for
+    column in edge[edge.notnull()].index])
+nx.draw(G,
+        node_size=1300,
+        font_color='blue',
+        node_color = 'white')
+plt.show()
